82f769aaf31a8221a2d2350fa08e323260b9cf1b
Umang kumar <umangkumarumk@gmail.com>
2024-02-11 15:52:29 +0530

    Changed App.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import dotenv from 'dotenv'
 import connectToDB from './db/dbConnect.js'
 import app from './app.js'
 
+
 dotenv.config({
     path: '/.env'
 })
 
 connectToDB()
 .then(() => {
+    app.on("error", (error) => {
+        console.log("Error", error);
+        throw error
+    })
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is listening on: ${process.env.PORT}`);
     })
